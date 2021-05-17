45e2976ec7ea2d0d2d6008a36142603171173337
Parth Parikh <parthparikh03@gmail.com>
2021-05-17 12:25:19 +0530

    navbar
    navbar half done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
